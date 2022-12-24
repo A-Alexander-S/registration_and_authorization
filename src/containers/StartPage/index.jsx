82bda0b689cefe3f59b8921410fbc9e609202f3f
Alexander <impoossible.777@gmail.com>
2022-12-24 17:50:35 +0400

    fonts,makeup: button, usercard

--- src/containers/StartPage/index.jsx
+++
@@ -1,9 +0,0 @@
-import './start-page.scss';
-
-const StartPage = () => {
-  return (
-    <div className="start-page">Ура</div>
-  )
-}
-
-export default StartPage;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import App from "../containers/App";
-import StartPage from "../containers/StartPage";
+import UsersPage from "../containers/UsersPage";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<App />}>
-        <Route index element={<StartPage />} />
+        <Route index element={<UsersPage />} />
       </Route>
     </Routes>
   )
